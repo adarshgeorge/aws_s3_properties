56352830f53e88d256f62353ff8851199fcff4ee
EC2 Default User <ec2-user@ip-172-31-4-240.ap-south-1.compute.internal>
2021-02-01 13:34:01 +0000

    fun

--- script_all.sh
+++ script_all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 script_aws () {
-
 	
-        for i in `aws s3 ls | cut -c21-250` ;do ./script.sh $i ; done
+        for i in `aws s3 ls | cut -c21-250` ; 
+	do ./script.sh $i ;
+       	done
 }
 script_aws
